7b8d6f8096c8945db7f9bf6261a26e0b422832b6
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-13 13:23:42 -0700

    mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,11 @@ import { INCREMENT, DECREMENT } from '../actions';
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
+
+// instead of a sep const for default state
+// added to reducer to export as a whole.
+// But is this something that won't work so well in larger scale prods?
+
 export default (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
